a62178223d08292db5f50ab70e7aae94c288a4f5
Julimar Junior <julimargomesjr@hotmail.com>
2023-05-07 01:52:16 -0300

    Middlewares em pasta fora de Http

--- middlewares.php
+++ middlewares.php
@@ -1,6 +1,8 @@
 <?php
-    use \App\Http\Middleware\Queue AS MiddlewareQueue;
+    use \App\Http\Middleware;
 
-    MiddlewareQueue::setMap([]);
+    Middleware::setMap([
+        'maintenance' => \App\Middlewares\Maintenance::class
+    ]);
 
-    MiddlewareQueue::setDefault([]);
+    Middleware::setDefault([]);
--- routes/pages.php
+++ routes/pages.php
@@ -2,6 +2,7 @@
     use \App\Controllers\Pages;
 
     $router->get('/', [
+        'middlewares' => ['maintenance'],
         function($request, $response) {
             return Pages\Example::getPage($request, $response);
         }
