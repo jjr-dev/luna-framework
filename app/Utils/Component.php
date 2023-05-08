422832304c28c128a6855e89dfc7dc6e10531d63
Julimar Jr <jjunior@xcapitalbank.com.br>
2023-05-08 20:12:09 -0300

    Componentes nas views e atualização de URI

---
+++ app/Utils/Component.php
@@ -0,0 +1,14 @@
+<?php
+    namespace App\Utils;
+
+    class Component extends View {
+        private static function getContentComponent($component) {
+            $file = __DIR__ . '/../../resources/components/' . $component . '.html';
+            return file_exists($file) ? file_get_contents($file) : '';
+        }
+        
+        public static function render($component, $vars = []) {
+            $contentComponent = self::getContentComponent($component);
+            return parent::render(false, $vars, $contentComponent);
+        }
+    }
