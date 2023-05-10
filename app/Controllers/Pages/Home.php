9e803f5b83ae13b85acef734bb9d0aba2886f37a
Julimar Junior <julimargomesjr@hotmail.com>
2023-05-10 00:29:55 -0300

    Correção de título

--- app/Controllers/Pages/Home.php
+++ app/Controllers/Pages/Home.php
@@ -6,7 +6,7 @@
     class Home extends Page {
         public static function getPage($req, $res) {
             $content = View::render('pages/home');
-            $content = parent::getPage("Luna- Framework MVC", $content);
+            $content = parent::getPage("Luna - Framework MVC", $content);
             
             return $res->send(200, $content);
         }
