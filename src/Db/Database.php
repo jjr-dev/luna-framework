88a10cd9ae01877f9b45cc60a5dbb31183771df0
Julimar Junior <julimargomesjr@hotmail.com>
2024-06-09 23:03:26 -0300

    Migração com CLI

--- src/Db/Database.php
+++ src/Db/Database.php
@@ -1,43 +1,62 @@
 <?php
-    namespace Luna\Db;
+namespace Luna\Db;
 
-    use Luna\Utils\Environment;
-    use Illuminate\Database\Capsule\Manager as Capsule;
+use Luna\Utils\Environment;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Schema\Blueprint;
 
-    class Database {
-        private static $configs = [];
+class Database {
+    private static $configs = [];
+    
+    private static function setConfigs() {
+        self::$configs = [
+            'driver'    => Environment::get('DB_DRIVER'),
+            'host'      => Environment::get('DB_HOST'),
+            'port'      => Environment::get('DB_PORT'),
+            'database'  => Environment::get('DB_NAME'),
+            'username'  => Environment::get('DB_USER'),
+            'password'  => Environment::get('DB_PASS'),
+            'charset'   => Environment::get('DB_CHARSET'),
+            'collation' => Environment::get('DB_COLLATION'),
+            'prefix'    => Environment::get('DB_PREFIX')
+        ];
+    }
+
+    public static function boot() {
+        self::setConfigs();
         
-        private static function setConfigs() {
-            self::$configs = [
-                'driver'    => Environment::get('DB_DRIVER'),
-                'host'      => Environment::get('DB_HOST'),
-                'database'  => Environment::get('DB_NAME'),
-                'username'  => Environment::get('DB_USER'),
-                'password'  => Environment::get('DB_PASS'),
-                'charset'   => Environment::get('DB_CHARSET'),
-                'collation' => Environment::get('DB_COLLATION'),
-                'prefix'    => Environment::get('DB_PREFIX')
-            ];
+        $capsule = new Capsule;
+        $capsule->addConnection(self::$configs);
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+
+        if(!Capsule::schema()->hasTable('migrations')) {
+            Capsule::schema()->create('migrations', function (Blueprint $table) {
+                $table->increments('id');
+                $table->text('filename');
+                $table->integer('batch');
+            });
         }
 
-        public static function boot($global = false) {
-            self::setConfigs();
-            
-            $capsule = new Capsule;
-            $capsule->addConnection(self::$configs);
-            $capsule->bootEloquent();
-
-            if($global) $capsule->setAsGlobal();
-        }
-
-        public static function queryToSql($query, $replaceBindings = false) {
-            $sql = $query->toSql();
-
-            if($replaceBindings)
-                foreach ($query->getBindings() as $binding) {
-                    $sql = preg_replace('/\?/', "'$binding'", $sql, 1);
-                }
-            
-            return $sql;
+        if(!Capsule::schema()->hasTable('logs')) {
+            Capsule::schema()->create('logs', function (Blueprint $table) {
+                $table->id();
+                $table->string('public_id');
+                $table->text('message');
+                $table->tinyText('code')->nullable();
+                $table->timestamp('created_at');
+            });
         }
     }
+
+    public static function queryToSql($query, $replaceBindings = false) {
+        $sql = $query->toSql();
+
+        if($replaceBindings)
+            foreach ($query->getBindings() as $binding) {
+                $sql = preg_replace('/\?/', "'$binding'", $sql, 1);
+            }
+        
+        return $sql;
+    }
+}
--- src/Db/Migration.php
+++ src/Db/Migration.php
@@ -1,34 +1,7 @@
 <?php
 namespace Luna\Db;
 
-class Migration {
-    public static function create($dir, $name) {
-        $time = floor(microtime(true) * 1000);
-        $filename = $time . '-' . $name . '.php';
-
-        file_put_contents(
-            $dir . $filename, 
-            self::arrayToText([
-                '<?php',
-                'use Illuminate\Support\Facades\Schema;',
-                'use Illuminate\Database\Schema\Blueprint;',
-                '',
-                'return new class {',
-                '    public function up() {',
-                '        Schema::create("name", function (Blueprint $table) {',
-                '            $table->increments("id");',
-                '        });',
-                '    }',
-                '',
-                '    public function down() {',
-                '        Schema::dropIfExists("name");',
-                '    }',
-                '};'
-            ])
-        );
-    }
-
-    public static function arrayToText($arr) {
-        return implode(PHP_EOL, $arr);
-    }
+class Migration extends Model {
+    protected $fillable = ["filename", "batch"];
+    public $timestamps = false;
 }
